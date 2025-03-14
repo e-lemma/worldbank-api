2a188827d61fce84bc63f1efbbebc06600d4fbc8
eyuale <157725293+e-lemma@users.noreply.github.com>
2025-03-14 14:29:50 +0000

    Modify Jest config for ESM support

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,18 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
-export const preset = 'ts-jest'
-export const testEnvironment = 'node'
-export const roots = ['<rootDir>/src/', '<rootDir>/tests/']
-export const transform = {
-  '^.+\.tsx?$': ['ts-jest', {}]
-}
-export const testRegex = '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$'
-export const moduleFileExtensions = ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
-export const collectCoverage = true
-export const coverageReporters = ['text', 'lcov']
-export const coverageDirectory = './coverage'
-export const coveragePathIgnorePatterns = [
-  '/node_modules/',
-  '/dist/',
-  '/tests/'
-]
-export const moduleNameMapper = {
-  '(.+)\\.js': '$1'
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src/', '<rootDir>/tests/'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {}]
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: true,
+  coverageReporters: ['text', 'lcov'],
+  coverageDirectory: './coverage',
+  coveragePathIgnorePatterns: ['/node_modules/', '/dist/', '/tests/'],
+  moduleNameMapper: {
+    '(.+)\\.js': '$1'
+  }
 }
