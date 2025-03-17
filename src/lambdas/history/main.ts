4c314d691f576cabce8f8caa20541ca96ecca7aa
eyuale <157725293+e-lemma@users.noreply.github.com>
2025-03-17 11:18:05 +0000

    Add basic structure

--- src/lambdas/history/main.ts
+++ src/lambdas/history/main.ts
@@ -1,6 +1,25 @@
-/**
- * verify access key
- * authenticate user
- * get user id from user table via user email
+import { DynamoDBClient, GetItemCommand } from '@aws-sdk/client-dynamodb'
+
+const client = new DynamoDBClient({})
+
+export const handler = async (event) => {
+  try {
+    /**
+     * get access key from query params
+     * validate access key
+     * get userid/user email from query params
+     * validate userid/user email
+     *  * get user id from user table via user email
  * use userid as history table pk to retrieve all data
- */
+
+     * get corresponding data from history table
+     * return it
+     */
+  } catch (error) {
+    console.error('Error:', error)
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal Server Error' })
+    }
+  }
+}
