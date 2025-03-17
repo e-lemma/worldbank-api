580fe337463d92936dc304efa47d1ae4fa3180e7
Gabriel <gabrielnsiah2011@gmail.com>
2025-03-17 15:12:29 +0000

    Lambda Handler finished

--- src/lambdas/countries/search_lambda.ts
+++ src/lambdas/countries/search_lambda.ts
@@ -4,51 +4,6 @@ import { Handler, APIGatewayProxyEvent, APIGatewayProxyEventQueryStringParameter
 import dotenv from 'dotenv'
 
 dotenv.config()
-const mockRequest = {resource: '/pets/search',
-path: '/pets/search',
-httpMethod: 'GET',
-headers: 
-{ Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-'Accept-Encoding': 'gzip, deflate, br',
-'Accept-Language': 'en-US,en;q=0.9',
-Host: 'xyz.execute-api.us-east-1.amazonaws.com',
-'Upgrade-Insecure-Requests': '1',
-'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
-Via: '1.1 382909590d138901660243559bc5e346.cloudfront.net (CloudFront)',
-'X-Amz-Cf-Id': 'motXi0bgd4RyV--wvyJnKpJhLdgp9YEo7_9NeS4L6cbgHkWkbn0KuQ==',
-'X-Amzn-Trace-Id': 'Root=1-5bab7b8b-f1333fbc610288d200cd6224',
-'X-Forwarded-Proto': 'https' },
-queryStringParameters: {IndicatorCode: "NY.ADJ.DFOR.CD", Year: "2013"},
-multiValueQueryStringParameters: { CountryCode: [ 'MDG', 'RUS' ] },
-pathParameters: null,
-stageVariables: null,
-requestContext: 
-{ resourceId: 'jy2rzf',
-resourcePath: '/pets/search',
-httpMethod: 'GET',
-extendedRequestId: 'N1A9yGxUoAMFWMA=',
-requestTime: '26/Sep/2018:12:28:59 +0000',
-path: '/staging/pets/search',
-protocol: 'HTTP/1.1',
-stage: 'staging',
-requestTimeEpoch: 1537964939459,
-requestId: 'be70816e-c187-11e8-9d99-eb43dd4b0381',
-apiId: 'xxxx' },
-body: null,
-isBase64Encoded: false }
-
-export const handler: Handler =  async(event: APIGatewayProxyEvent) => {
-    try {
-        const parameters = mockRequest.multiValueQueryStringParameters
-        const countryData = []
-        for (const country in parameters)
-            console.log(country)
-
-    } catch (error) {
-        return null
-    }
-}
-
 function connectToClient(){
     const client = new DynamoDBClient({
         region: process.env.AWS_REGION,
@@ -62,7 +17,6 @@ function connectToClient(){
 
     return docClient
 }
-
 async function scanCountryIndicators(filters: APIGatewayProxyEventQueryStringParameters, docClient: DynamoDBDocumentClient) {
   const filterExpressions: string[] = []
   const expressionAttributeValues: Record<string, unknown> = {}
@@ -101,22 +55,35 @@ async function scanCountryIndicators(filters: APIGatewayProxyEventQueryStringPar
   return response.Items
 }
 
-async function main(){
-    const docClient = connectToClient()
-    const countries = mockRequest.multiValueQueryStringParameters.CountryCode
-    const parameters = mockRequest.queryStringParameters
-    const data = []
-    for (const country of countries) {
-        Object.assign(parameters, {CountryCode: country})
-        const countryData = await scanCountryIndicators(parameters, docClient)
-        data.push(countryData)
+export const handler: Handler =  async(event: APIGatewayProxyEvent) => {
+    try {
+        const docClient = connectToClient()
+        const countries = event.multiValueQueryStringParameters?.CountryCode
+        const parameters = event.queryStringParameters!
+        const data = []
+        if (countries) {
+            for (const country of countries) {
+            const countryParams = { ...parameters, CountryCode: country }
+            const countryData = await scanCountryIndicators(countryParams, docClient)
+            data.push(countryData)
+            }
+        } else {
+            const countryData = await scanCountryIndicators(parameters, docClient)
+            data.push(countryData)
+        }
+       return {
+            statusCode: 200,
+            body: JSON.stringify(data.flat()),
+        }
         
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: error }),
+        }
     }
-    console.log(data.flat())
-
 }
 
-main()
 
 
 
