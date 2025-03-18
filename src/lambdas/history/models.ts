15e5cd0911139dc4a0a80393ed9bb8b638e56f0c
eyuale <157725293+e-lemma@users.noreply.github.com>
2025-03-18 12:32:51 +0000

    Change `SearchHistory` and `User` classes into interfaces

--- src/lambdas/history/models.ts
+++ src/lambdas/history/models.ts
@@ -1,40 +1,13 @@
-export class SearchHistory {
+export interface SearchHistory {
   userId: string // Partition key
   timestamp: string // Sort key
   queryType: string
   parameters: Record<string, string[]> // Countries, Indicators
-
-  constructor(data: {
-    userId: string
-    timestamp: string
-    queryType: string
-    parameters: Record<string, string[]>
-  }) {
-    this.userId = data.userId
-    this.timestamp = data.timestamp
-    this.queryType = data.queryType
-    this.parameters = data.parameters
-  }
 }
 
-export class User {
+export interface User {
   email: string // Partition Key
   userId: string
   password: string
-  lastLogin: string
   createdAt: string
-
-  constructor(data: {
-    email: string
-    userId: string
-    password: string
-    lastLogin: string
-    createdAt: string
-  }) {
-    this.email = data.email
-    this.userId = data.userId
-    this.password = data.password
-    this.lastLogin = data.lastLogin
-    this.createdAt = data.createdAt
-  }
 }
