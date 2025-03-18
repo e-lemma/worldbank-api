d682001d2d75b99c35209bcc6ad561938bf6f246
eyuale <157725293+e-lemma@users.noreply.github.com>
2025-03-18 12:33:26 +0000

    Add main functionality for Lambda function handler

--- src/lambdas/history/main.ts
+++ src/lambdas/history/main.ts
@@ -1,18 +1,50 @@
-import { DynamoDBClient, GetItemCommand } from '@aws-sdk/client-dynamodb'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import { configDotenv } from 'dotenv'
 
-export const handler = async (event: any) => {
+import { AccessKeyRetriever } from '../api_key_retriever'
+import { UserService, HistoryService } from './services'
+
+export const handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
   try {
-    /**
-     * get access key from query params
-     * validate access key
-     * get userid/user email from query params
-     * validate userid/user email
-     *  * get user id from user table via user email
- * use userid as history table pk to retrieve all data
+    configDotenv()
 
-     * get corresponding data from history table
-     * return it
-     */
+    const apiKey = event.queryStringParameters?.api_key
+    if (!apiKey || apiKey !== (await AccessKeyRetriever.retrieveApiKey())) {
+      return {
+        statusCode: 401,
+        body: JSON.stringify({
+          error: 'Unauthorised',
+          message: 'Missing or invalid API key'
+        })
+      }
+    }
+    const userEmail = event.queryStringParameters?.email
+    const userPass = event.queryStringParameters?.password
+    const userService = new UserService(process.env.AWS_REGION)
+    if (
+      !userEmail ||
+      !userPass ||
+      !(await userService.authenticateCredentials(userEmail, userPass))
+    ) {
+      return {
+        statusCode: 401,
+        body: JSON.stringify({
+          error: 'Unauthorised',
+          message: 'Missing or invalid account credentials'
+        })
+      }
+    }
+    const historyService = new HistoryService(process.env.AWS_REGION)
+    const matchingUserId = await historyService.getUserId(userEmail)
+    const historyData = historyService.getUserHistory(matchingUserId)
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: historyData
+      })
+    }
   } catch (error) {
     console.error('Error:', error)
     return {
